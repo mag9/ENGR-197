1a2a82bfffdc5aa0c72ff78b78f5e6af48422f19
Jeremy Stark <razestudiosdev@gmail.com>
2018-09-06 19:42:23 -0400

    Finished HW_01_C
    Finished HW_01_C

--- Homework/HW_1/HW_01_C/stark_hw_01_c.c
+++ Homework/HW_1/HW_01_C/stark_hw_01_c.c
@@ -8,23 +8,26 @@ int main(void)
 	int cylHeight;
 	int coneHeight;
 
-	double volumeCone;
 	double volumeCylinder;
+	double volumeCone;
 	double volumeTotal;
 
 	printf("Enter radius of bottle:\n");
-	scanf("%d", radius);
+	scanf("%d", &radius);
 
 	printf("Enter height of cylindrical section:\n");
-	scanf("%d", cylHeight);
+	scanf("%d", &cylHeight);
 
 	printf("Enter the height of the conic section:\n");
-	scanf("%d", coneHeight);
+	scanf("%d", &coneHeight);
 
-	volumeCylinder = PI * pow(radius, 2) * cylHeight;
-	volumeCone = (1/3) * PI * pow(radius, 2) * cylHeight;
+	volumeCylinder = (PI * pow(radius, 2) * cylHeight);
+	volumeCone = ((1.f/3) * PI * pow(radius, 2) * cylHeight);
+	volumeTotal = volumeCone + volumeCylinder;
 
-	printf("Cylinder:%0.4f", );
+	printf("Cylinder Volume: %0.4f\n", volumeCylinder);
+	printf("Cone Volume: %0.4f\n", volumeCone);
+	printf("Total Volume: %0.4f\n", volumeTotal);
 
 	volumeTotal = volumeCone + volumeCylinder;
 

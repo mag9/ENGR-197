311471ab5faebbc8c3677b44d0899b10d6c39adc
Jeremy Stark <razestudiosdev@gmail.com>
2018-10-01 19:35:55 -0400

    Update to CW_12
    +Added comments
    +Added screenshot of test run

--- Classwork/CW_12/stark_cw_12.c
+++ Classwork/CW_12/stark_cw_12.c
@@ -1,6 +1,15 @@
+/*
+CW_12
+Jeremy Stark
+10/02/2018
+A program will calculate the speed of
+sound in air at a specific temperature.
+*/
+
 #include <stdio.h>
 #include <math.h>
 
+//Function that takes in temperature (in F), calculates the speed, and returns as a double.
 double soundSpeed(int temp)
 {
 	double speed = sqrt((5*temp+297)/247);
@@ -8,20 +17,27 @@ double soundSpeed(int temp)
 	return speed;
 }
 
+//Function that takes in Temp (in F), calculated speed, and prints out the final input including the two.
 void printOut(int temp, double speed)
 {
 	printf("Speed of sound is %0.2lf at %dF\n", speed, temp);
 }
 
+//Main function
 int main(void)
 {
+	//Temp and speed variable
 	int temp;
 	double speed;
 
+	//Prompts user to enter temperature and scans it into variable, "temp".
 	printf("Please enter temperature: \n");
 	scanf("%d", &temp);
 
+	//Calls speed function to set value of speed variable.
 	speed = soundSpeed(temp);
+
+	//Calls printOut taking in temp and speed variable to print out formatted output.
 	printOut(temp, speed);
 
 	return 0;

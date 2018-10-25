db5a50ed8da45559bac4d248f08e68a7aea72dbb
Jeremy Stark <razestudiosdev@gmail.com>
2018-10-25 16:47:39 -0400

    Finished HW_05

--- Homework/HW_05/HW_05_B/stark_hw_05_b.c
+++ Homework/HW_05/HW_05_B/stark_hw_05_b.c
@@ -1,5 +1,5 @@
 /*
-HW_05_A
+HW_05_B
 Jeremy Stark
 10/25/2018
 A program that calculate the arithmetic mean,
@@ -13,24 +13,47 @@ for a set of non-zero numbers.
 //Main Method
 int main(void)
 {
+  //Variables
   double num;
   double arithPartial;
-  double geomPartial;
+  double geomPartial = 1;
   double harmPartial;
   double arithmetic;
   double geometric;
   double harmonic;
+  double n;
 
+  //Prompts users to enter numbers between 0 and 10, and -99 when finished.
   printf("Please enter numbers between 0 and 10. Enter -99 when finished: \n");
 
+  /*
+    While loop for taking input
+
+    Will run while num entered is not -99
+  */
   while(num != SENTINAL)
   {
-    scanf("%lf", num);
+    scanf("%lf", &num);
 
-    arithPartial = arithPartial + num;
-    geomPartial = geomPartial * num;
-    harmPartial = harmPartial + (1/num);
+    //Will only run the methods for calculating means if num is not -99.
+    if(num != SENTINAL)
+    {
+      n++;
+
+      //Calculates the partial sums used within the mean calculations.
+      arithPartial = arithPartial + num;
+      geomPartial = geomPartial * num;
+      harmPartial = harmPartial + (1/num);
+    }
   }
+
+  //Calculates the final mean values.
+  arithmetic = arithPartial / n;
+  geometric = pow(geomPartial, 1/n);
+  harmonic = n / harmPartial;
+
+  //Prints out the final mean values.
+  printf("Arithmetic: %0.3lf\nGeometric: %0.3lf\nHarmonic: %0.3lf\n", arithmetic, geometric, harmonic);
 
   return 0;
 }
---
+++ Homework/HW_05/testhw.c
@@ -0,0 +1,70 @@
+/*
+HW_05_B
+Jeremy Stark
+10/25/2018
+A program that calculate the arithmetic mean,
+the geometric mean, and the harmonic mean
+for a set of non-zero numbers.
+*/
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+  int num[5];
+  double mean;
+  int min = 100;
+  int max = 0;
+  int numEven = 0;
+  int numOdd = 0;
+  int sum = 0;
+
+  srand(time(NULL));
+
+  //Generates and populates int array with 5 random values
+  for(int i = 0; i < 5; i++)
+  {
+    num[i] = 0 + rand() % (100 + 1 - 0);
+  }
+
+  //Prints out the generated values;
+  printf("Numbers: ");
+  for(int i = 0; i < 5; i++)
+  {
+    printf("%d ", num[i]);
+  }
+  printf("\n");
+
+  for(int i = 0; i < 5; i++)
+  {
+    sum = num[i];
+
+    if(num[i] < min)
+    {
+      min = num[i];
+    }
+
+    if(num[i] > max)
+    {
+      max = num[i];
+    }
+
+    if(num[i] % 2 == 0)
+    {
+      numEven++;
+    }
+    else
+    {
+      numOdd++;
+    }
+  }
+  mean = ((double)sum / 5);
+  printf("Mean: %0.2lf\n", mean);
+  printf("Min: %0.2lf\n", min);
+  printf("Max: %0.2lf\n", max);
+  printf("Number of Even: %0.2lf\n", numEven);
+  printf("Number of Odd: %0.2lf\n", numOdd);
+
+  return 0;
+}
